c953bc2c9a5995901d8dc08112b56b8487ee1ce8
Juliana Meneguete <jumeneguete@gmail.com>
2021-07-22 19:19:03 -0300

    add a recommendation - tests

--- src/services/recommendationService.ts
+++ src/services/recommendationService.ts
@@ -9,7 +9,6 @@ export async function create(name: string , link: string){
     await recommendationRepository.addRecommendation(name, link);
 }
 
-
 function isYoutubeVideo(url: string) {
   var v = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
   return (url.match(v)) ? RegExp.$1 : false;
--- tests/integration/test.test.ts
+++ tests/integration/test.test.ts
@@ -1,10 +1,57 @@
+import "../../src/setup.ts";
 import supertest from "supertest";
 import app from "../../src/app";
+import connection from "../../src/database"
 
-describe("GET /test", () => {
-  it("should answer with text \"OK!\" and status 200", async () => {
-    const response = await supertest(app).get("/test");
-    expect(response.text).toBe("OK!");
-    expect(response.status).toBe(200);
+beforeEach(async () => {
+  await connection.query('TRUNCATE songs RESTART IDENTITY');
+});
+
+afterAll(async () => {
+  await connection.end();
+});
+
+const agent = supertest(app);
+
+describe("POST /recommendations", () => {
+  it("should return status 201 when receive a valid body", async () => {
+    const body = {
+      name: "Valid String 01",
+      youtubeLink: "https://www.youtube.com/watch?v=eitDnP0_83k&list=RDu04baUNhhy0&index=4"
+    }
+
+    const response = await agent.post("/recommendations").send(body);
+
+    expect(response.status).toBe(201);
+  });
+
+  it("should return status 400 when receive a invalid body", async () => {
+    const body = {};
+
+    const response = await agent.post("/recommendations").send(body);
+
+    expect(response.status).toBe(400);
+  });
+
+  it("should return status 406 when name is not a string", async () => {
+    const body = {
+      name: 1234,
+      youtubeLink: "https://www.youtube.com/watch?v=eitDnP0_83k&list=RDu04baUNhhy0&index=4"
+    }
+
+    const response = await agent.post("/recommendations").send(body);
+
+    expect(response.status).toBe(406);
+  });
+
+  it("should return status 406 when youtubeLink is not a link from youtube", async () => {
+    const body = {
+      name: "Valid String 01",
+      youtubeLink: "https://www.globo.com"
+    }
+
+    const response = await agent.post("/recommendations").send(body);
+
+    expect(response.status).toBe(406);
   });
 });
