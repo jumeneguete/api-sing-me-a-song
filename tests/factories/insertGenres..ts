0450e275de3d35e3c1a32acb57914c3c58c1a22f
thiagoribb <thiribeiro142@gmail.com>
2021-07-27 15:45:10 -0300

    preparando review

--- tests/factories/insertGenres..ts
+++
@@ -1,5 +0,0 @@
-import connection from "../../src/database";
-
-export async function insertGenre(body: {name: string}){
-    await connection.query(`INSERT INTO genres (name) VALUES ($1)`, [body.name]);
-}
--- tests/factories/updateSong.ts
+++
@@ -1,5 +0,0 @@
-import connection from "../../src/database";
-
-export async function updateRecommendation(id: number){
-    return  await connection.query(`UPDATE songs SET score = -5 WHERE id= ${id}`);
-}
